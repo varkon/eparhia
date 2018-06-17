9fbc3b73e009f7cc8e2405fd61e110a477d2450f
Kostiantyn Malakhov <info@varkon.biz>
2018-06-17 17:32:33 +0300

    Add deanery model

--- tamples/models.py
+++ tamples/models.py
@@ -5,17 +5,42 @@ from filebrowser.fields import FileBrowseField
 from eparhiapp.apps import transliterate
 
 # Create your models here.
+class Deanery(models.Model):
+    title = models.CharField(max_length = 255, verbose_name = 'Назва')
+    abbot = models.CharField(max_length = 255, null= True, verbose_name = 'Благочинний')
+    description = HTMLField(null = True, verbose_name = 'Детальний опис')
+    link = models.CharField(max_length=255, verbose_name='Посилання', unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.createlink()
+        super(Deanery, self).save(*args, **kwargs)
+
+    def createlink(self):
+        if (self.link == ""):
+            self.link = transliterate(self.title)
+
+    def __str__(self):
+        return self.title
+
+class DeaneryContacts(models.Model):
+    deanery = models.ForeignKey(Deanery, on_delete=models.CASCADE)
+    contact = models.CharField(max_length=20, verbose_name='Телефон або инший контакт')
+    description = models.CharField(max_length=50, null=True, verbose_name='Примитки')
+
+    def __str__(self):
+        return self.phonenumber
+
+
 class Tample(models.Model):
     title = models.CharField(max_length = 255, verbose_name='Назва')
-    saint = models.CharField(max_length = 255, verbose_name='На честь святого')
     address = models.CharField(max_length = 255, verbose_name='Адреса')
-    city = models.CharField(max_length = 255, verbose_name='Місто')
+    city = models.ForeignKey(Deanery, on_update = models.CASCADE,on_delete = models.CASCADE, verbose_name = '', default = 1)
     body = HTMLField(verbose_name='Опис')
     abbot = models.CharField(max_length = 255, verbose_name='Настоятель')
     link = models.CharField(max_length=255, verbose_name='Посилання', unique=True, blank=True)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     created_date = models.DateField(
-        default=datetime.date.today)
+        default=datetime.date.today, null = True)
     published_date = models.DateField(
         blank=True, null=True)
     icon = FileBrowseField("Зображення", max_length=250, directory="uploads/", extensions=[".jpg", "jpeg", "png"],
--- tamples/views.py
+++ tamples/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-from .models import Tample
+from .models import Deanery,Tample
 
 # Create your views here.
 
+def deanery_list(request):
+    deanery = Deanery.objects.all()
+    if deanery.count() == 0 :
+        return tamples_list(request,1)
+    contacts = deanery.contacts_set.all()
+    return render(request, 'tamples/index.html',{'deanery':deanery, 'contacts':contacts})
 
-def tamples_list(request):
-    tamples = Tample.objects.all().order_by("created_date")
-    return render(request, 'tamples/index.html', {'tamples':tamples})
+def tamples_list(request, id):
+    tamples = Tample.objects.filter(city_id = id).order_by("created_date")
+    return render(request, 'tamples/index_tample.html', {'tamples':tamples})
 
 def detail_tample(request, path):
     post = Tample.objects.get(link = path)
