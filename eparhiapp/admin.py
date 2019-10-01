279879e60458a293da778bb9913319e9494b8424
Kostiantyn Malakhov <info@varkon.biz>
2019-10-01 22:30:53 +0300

    Add benefactors

--- eparhiapp/admin.py
+++ eparhiapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Patriarch, Archbishop, Primat, About
+from .models import Patriarch, Archbishop, Primat, About, Benefactor
 # Register your models here.
 
 admin.site.register(Patriarch)
 admin.site.register(Archbishop)
 admin.site.register(Primat)
 admin.site.register(About)
+admin.site.register(Benefactor)
