0c047ac267b0296fbd7c33e9c1d74db40a15f41f
Kostiantyn Malakhov <info@varkon.biz>
2019-11-05 15:06:27 +0200

    Add new media

--- tamples/admin.py
+++ tamples/admin.py
@@ -12,7 +12,13 @@ class ContactsInLine(admin.TabularInline):
     extra = 1
 
 class TampleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'get_deanery']
     inlines = [PhoneInline]
+    def get_deanery(self, obj):
+        return obj.Deanery.title
+
+    get_deanery.admin_order_field = 'title'  # Allows column order sorting
+    get_deanery.short_description = 'Благочиння'  # Renames column head
 
 class DeaneryAdmin(admin.ModelAdmin):
     inlines = [ContactsInLine]
